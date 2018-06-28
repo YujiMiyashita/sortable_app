b9b2b417c1b47f94978065d1c05f042baa7e1fc1
Yuji Miyashita <ikkiiimonkey0229@gmail.com>
2018-06-28 20:02:32 +0900

    add RankedModel

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628105112) do
+ActiveRecord::Schema.define(version: 20180628105707) do
 
   create_table "fruits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "row_order"
   end
 
 end
